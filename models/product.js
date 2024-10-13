36a902a9d53ea0561b839129603d221f4c385be2
akaphudz <konamiid2808@gmail.com>
2024-10-13 17:31:34 +0700

    express project

---
+++ models/product.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose")
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+});
+
+module.exports = mongoose.model('Product', productSchema);
+
+
